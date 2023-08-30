770d0ebca6675ba3af1f0f11c9c9a9df64ce37b7
DawidRaczek <dawid.raczek6666@gmail.com>
2023-08-30 17:23:00 +0200

    added styling in CSS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 function calculate() {
-    var amount = parseFloat(document.getElementById("amount").value);
-    var fromCurrency = document.getElementById("fromCurrency").value;
-    var toCurrency = document.getElementById("toCurrency").value;
+    let amount = parseFloat(document.getElementById("amount").value);
+    let fromCurrency = document.getElementById("fromCurrency").value;
+    let toCurrency = document.getElementById("toCurrency").value;
 
     if (fromCurrency === "PLN") {
         fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${toCurrency}/`)
             .then(response => response.json())
             .then(data => {
-                var toRate = data.rates[0].mid;
-                var result = amount / toRate;  // Poprawka tutaj
+                let toRate = data.rates[0].mid;
+                let result = amount / toRate;  // Poprawka tutaj
                 document.getElementById("result").innerHTML = result.toFixed(2) + " " + toCurrency;
             })
             .catch(error => {
@@ -18,14 +18,14 @@ function calculate() {
         fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${fromCurrency}/`)
             .then(response => response.json())
             .then(data => {
-                var fromRate = data.rates[0].mid;
+                let fromRate = data.rates[0].mid;
 
                 fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${toCurrency}/`)
                     .then(response => response.json())
                     .then(data => {
-                        var toRate = data.rates[0].mid;
+                        let toRate = data.rates[0].mid;
 
-                        var result = (amount / fromRate) * toRate;
+                        let result = (amount / fromRate) * toRate;
                         document.getElementById("result").innerHTML = result.toFixed(2) + " " + toCurrency;
                     })
                     .catch(error => {
